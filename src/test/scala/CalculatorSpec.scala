4a96164c534f8421f0f20931ea328c115a04c038
fjasensi <fjasensi@outlook.com>
2024-05-20 20:50:09 +0200

    Añadir test para la operación de suma

--- src/test/scala/CalculatorSpec.scala
+++ src/test/scala/CalculatorSpec.scala
@@ -2,5 +2,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CalculatorSpec extends AnyFlatSpec with Matchers {
-
+  "Calculator" should "sumar dos números correctamente" in {
+    val calculator = new Calculator
+    calculator.add(2, 3) shouldEqual 5
+  }
 }
