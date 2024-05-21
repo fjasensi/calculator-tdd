5954862569dd341702a0d9b2d3d4de239814fc72
fjasensi <fjasensi@outlook.com>
2024-05-21 20:52:50 +0200

    Test para la operación de raíz cuadrada Test para no hacer raíces cuadradas de números negativos

--- src/test/scala/CalculatorSpec.scala
+++ src/test/scala/CalculatorSpec.scala
@@ -26,4 +26,17 @@ class CalculatorSpec extends AnyFlatSpec with Matchers {
     val calculator = new Calculator
     an[ArithmeticException] should be thrownBy calculator.divide(10, 0)
   }
+
+  it should "calcular la raíz cuadrada de un número con precisión 0.001" in {
+    val calculator = new Calculator
+    val result = calculator.sqrt(2)
+    val expectedResult = 1.414213562
+    
+    result shouldEqual expectedResult +- 0.001
+  }
+  
+  it should "lanzar ArithmeticException cuando calculamos la raíz cuadrada de un número negativo" in {
+    val calculator = new Calculator
+    an[ArithmeticException] should be thrownBy calculator.sqrt(-2)
+  }
 }
