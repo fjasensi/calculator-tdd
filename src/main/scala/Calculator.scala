2ea90061f118fdf63691466f579368928e17e4fe
fjasensi <fjasensi@outlook.com>
2024-05-21 20:39:09 +0200

    Implementar el método de dividir

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -4,4 +4,9 @@ class Calculator {
   def subtract(a: Float, b: Float): Float = a - b
 
   def multiply(a: Float, b: Float): Float = a * b
+
+  def divide(a: Float, b: Float): Float = {
+    if (b == 0) throw new ArithmeticException("Division by zero")
+    a / b
+  }
 }
